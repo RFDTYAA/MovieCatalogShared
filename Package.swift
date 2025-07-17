af48093e7315f6e262331f116668535278eff206
Muhammad Rafi Aditya <mrafiaditya04@gmail.com>
2025-07-17 10:38:55 +0700

    fix: Simplify package structure

--- Package.swift
+++ Package.swift
@@ -12,16 +12,13 @@ let package = Package(
             targets: ["Shared"]),
     ],
     dependencies: [
-        // Dependensi yang digunakan oleh modul Shared
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0")
     ],
     targets: [
         .target(
             name: "Shared",
             dependencies: ["Kingfisher"],
-            path: "Shared", // Lokasi source code
             resources: [
-                // Daftarkan folder Assets agar bisa digunakan
                 .process("Assets.xcassets")
             ]
         )
